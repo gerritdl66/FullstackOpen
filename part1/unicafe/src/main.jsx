2b591e6d77d1633ca2335e9ba8fe086257fbaac4
Gerrit De Lange <gerritdl@towerquip.co.za>
2025-09-26 08:35:42 +0200

    adding unicafe files

---
+++ part1/unicafe/src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDom from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+    <App />
+  
+)
