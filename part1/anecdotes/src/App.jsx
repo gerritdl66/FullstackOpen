a7bae5745144570664a9fb00263df6dcdc4a376c
Gerrit De Lange <gerritdl@towerquip.co.za>
2025-10-07 10:13:02 +0200

    Commint end of part1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,34 +1,87 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Titles = (props) => {
 
   return (
-    <>
+    <div>
+      <h1>{props.text}</h1>
+    </div>
+  )
+}
+
+
+const SelectAnec = (props) =>{
+// console.log(value);
+return(
+  <div>
+    {/* <h1>Anecdote of the day</h1> */}
+    <p>{props.value}</p>
+    <p>Has {props.valuevotes} votes</p>
+  </div>
+)
+}
+
+const Button = (props) => {
+  return (
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <button onClick={props.handleClick}>{props.text}</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )
+}
+
+
+
+
+
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+ 
+
+   
+  const [selected, setSelected] = useState(0)
+  const votes= Array(anecdotes.length).fill(0);
+  const [vote, setVote] = useState(votes);
+   
+ console.log("Vote1:", {vote}) 
+
+// console.log("Vote1:", {vote})
+
+
+const cntVotes = () => {
+  const newVotes = [...vote]
+  newVotes[selected] +=1
+    setVote(newVotes)
+  console.log("Vote2:", {vote})
+}
+
+
+// console.log(selected)
+  return (
+    
+    <div>
+      <Titles text="Anecdote of the day"/>
+      <SelectAnec value={anecdotes[selected]} valuevotes={vote[selected]}/>
+      <Button handleClick={cntVotes} text="Vote" />
+      <button onClick={()=> setSelected(Math.floor(Math.random()*7))}>Next Anecdote</button>
+      <Titles text="Most popular Anecdote"/>  
+      <SelectAnec value={anecdotes[vote.indexOf(Math.max(...vote))]} valuevotes={Math.max(...vote)}/>
+
+      
+      
+            
+    </div>
   )
 }
 
